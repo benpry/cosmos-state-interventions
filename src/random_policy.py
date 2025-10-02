88a2437286ef9ab0c0302d13632c677fb463f97c
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2025-10-02 14:34:04 +0900

    added conda env and random policy file

---
+++ src/random_policy.py
@@ -0,0 +1,13 @@
+import gymnasium as gym
+
+env = gym.make("FrozenLake-v1", render_mode="human")
+
+observation, info = env.reset(seed=42)
+for _ in range(100):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    env.render()
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
