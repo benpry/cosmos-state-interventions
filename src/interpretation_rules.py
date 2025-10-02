b805bbe9885beb036109c788a235b1ba89f404d7
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2025-10-02 16:25:06 +0900

    added interpretation rules skeleton

---
+++ src/interpretation_rules.py
@@ -0,0 +1,18 @@
+def suggestion():
+    # update q value for action closest to the intervention
+    pass
+
+
+def reset():
+    # get the reward of the new state, associate it with the action you would have taken
+    pass
+
+
+def interrupt():
+    # ignore the timestep entirely
+    pass
+
+
+def impede():
+    # fixed negative reward for whatever you were going to do beforehand
+    pass
